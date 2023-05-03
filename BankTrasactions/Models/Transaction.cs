b4dca1f3c9b14b746a2c0706c693ac8bea4e3aa1
MusliMuzaqi <mm200082k@riinvest.net>
2023-05-03 12:14:20 +0200

    Add project files.

---
+++ BankTrasactions/Models/Transaction.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTrasactions.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Required(ErrorMessage ="This field is required")]
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [MaxLength(12, ErrorMessage ="Maximum 12 characters only.")]
+        public string AccountNumber { get; set; }
+
+
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName ="nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        public string BeneficiaryName { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        public string BankName { get; set; }
+
+
+        [MaxLength(12, ErrorMessage = "Maximum 12 characters only.")]
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("Swift Code")]
+        public string SwiftCode { get; set; }
+
+
+        [Required(ErrorMessage = "This field is required")]
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ BankTrasactions/Models/TransactionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankTrasactions.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
